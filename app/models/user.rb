f608505fc99dcd593523a7d5cc32066ebcbdf168
Takahiro <tsolvalouy@gmail.com>
2017-05-03 06:09:30 +0000

    Create User model

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[\w\d\-.]+\.[A-z]+\z/
+
+   validates :name,        presence: true
+   validates :email,       presence: true,
+                           format: { with: VALID_EMAIL_REGEX }
+   validates :password,    presence: true,
+                           allow_nil: true,
+                           length: { minimum: 4 }
+   has_secure_password
+
+   def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+   end
+
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,60 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+   def setup
+      @user  = User.new(
+                         name:                   "例学　雅人",
+                         email:                  "example@gmail.com",
+                         password:               "password",
+                         password_confirmation:  "password"
+                        )
+   end
+
+   test "should be valid" do
+      assert @user.valid?
+   end
+
+   test "name should be present" do
+      @user.name = ""
+      assert_not @user.valid?
+   end
+
+   test "email should be present" do
+      @user.email = ""
+      assert_not @user.valid?
+   end
+
+    test "email should be in a proper format" do
+       valid_addresses = [
+                          "user@example.com",
+                          "user@mail.co.jp",
+                          "USER@foo.com",
+                          "foo_bar+oh@mail.com"
+                         ]
+       valid_addresses.each do |address|
+          @user.email = address
+          assert @user.valid?
+       end
+    end
+
+    test "password should be present" do
+       @user.password = ""
+       @user.password_confirmation = ""
+       assert_not @user.valid?
+    end
+
+    test "password should match with its confirmation" do
+       @user.password = "drowssap"
+       assert_not @user.valid?
+       @user.password_confirmation = @user.password
+       assert @user.valid?
+    end
+
+    test "password should have more than 4 chars" do
+       @user.password = @user.password_confirmation = "p" * 4
+       assert @user.valid?
+       @user.password = @user.password_confirmation = "p" * 3
+       assert_not @user.valid?
+    end
+end
