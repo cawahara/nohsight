2b81e824d06504ce54295b630f3df12e61fd454e
Takahiro <tsolvalouy@gmail.com>
2017-05-07 02:41:58 +0000

    Create Dashboard and Detail template

--- app/controllers/mockups_controller.rb
+++ app/controllers/mockups_controller.rb
@@ -1,12 +1,14 @@
 class MockupsController < ApplicationController
   def dashboard
-
+     @user = User.find(1)
+     @events = Event.all
   end
 
   def search
+
   end
 
   def detail
-     
+     @event = Event.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get '/dashboard',     to: 'mockups#dashboard'
+  get '/dashboard',     to: 'mockups#dashboard', as: 'dashboard'
 
-  get '/search/:id',    to: 'mockups#search'
+  get '/search/:id',    to: 'mockups#search', as: 'search'
 
-  get '/detail/:id',    to: 'mockups#detail'
+  get '/detail/:id',    to: 'mockups#detail', as: 'detail'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
