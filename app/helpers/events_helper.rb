44c7d9d0463d1d2e2d1a3176fcd4bd96f131a49e
Takahiro <tsolvalouy@gmail.com>
2017-05-19 14:56:07 +0000

    Create Detail view on Events controller

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -17,15 +17,17 @@ module EventsHelper
                   }
 
       # 緯度・経度を基に周辺スポットを3つまで取得
-      place_api_uri = URI.parse("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{geo_info['latitude']},#{geo_info['longitude']}&radius=500&keyword=temple&key=#{ENV['GOOGLE_API_KEY']}")
+      place_api_uri = URI.parse("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{geo_info['latitude']},#{geo_info['longitude']}&radius=1000&keyword=temple&key=#{ENV['GOOGLE_API_KEY']}")
       place_json = JSON.parse(Net::HTTP.get(place_api_uri))
 
-      # Google Custom Search API でurlと最初の説明のみ入手
+      # Google Custom Search API でurlを入手
       rec_places = []
       3.times do |n|
          place_title = place_json['results'][n]['name']
          if place_title != nil
-            place_thumbnail = Net::HTTP.get(URI.parse("https://maps.googleapis.com/maps/api/place/photo?photoreference=#{place_json['results'][n]['photos'][0]['photo_reference']}&key=#{ENV['GOOGLE_API_KEY']}"))
+            # FIXME 機能するが、コードが汚すぎる(主にGETリクエスト時のURLが冗長)短める必要性あり
+            place_thumbnail = Net::HTTP.get(URI.parse("https://maps.googleapis.com/maps/api/place/photo?&sensor=false&maxwidth=80&maxheight=80&photoreference=#{place_json['results'][n]['photos'][0]['photo_reference']}&key=#{ENV['GOOGLE_API_KEY']}"))
+               place_thumbnail = place_thumbnail.scan(/https:\/\/lh3\.googleusercontent.com.+"/).to_s.gsub(/"|\[|\]|\\/, '')
             place_url = JSON.parse(Net::HTTP.get(URI.parse("https://maps.googleapis.com/maps/api/place/details/json?&reference=#{place_json['results'][n]['reference']}&key=#{ENV['GOOGLE_API_KEY']}")))
             rec_place = {
                          "title" => place_title,
