2d6d2d73cd1871ab42ebffb4f68887ee9fdca6e2
Takahiro <tsolvalouy@gmail.com>
2017-05-21 09:13:08 +0000

    Edit User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
    def new
+      @user = User.new()
    end
 
    def show
@@ -10,6 +11,12 @@ class UsersController < ApplicationController
    end
 
    def create
+      @user = User.new(user_params)
+      if @user.save
+         redirect_to(dashboard_url)
+      else
+         render 'users/new'
+      end
    end
 
    def update
@@ -17,5 +24,10 @@ class UsersController < ApplicationController
 
    def destroy
    end
+
+   private
+      def user_params
+         params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
 
 end
