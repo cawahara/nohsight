ff4b5ab0dad46f7fa7a5f82526d23838d843df06
Takahiro <tsolvalouy@gmail.com>
2017-05-21 11:49:41 +0000

    Add agreement field for creating a new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
                            allow_nil: true,
                            length: { minimum: 4 }
    has_secure_password
+   # 利用規約に従う
+   validates_acceptance_of :agreement, allow_nil: false, on: :create
 
    def User.digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
