82509b6c0cfaede86a4224f9d012ff2da5f67b52
Takahiro <tsolvalouy@gmail.com>
2017-05-21 14:33:12 +0000

    Build Login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
  # StaticPage用ルーティング
-  get '/dashboard'      , to: 'static_pages#dashboard'   , as: 'dashboard'
-  get '/about'          , to: 'static_pages#about'       , as: 'about'
-  get '/help'           , to: 'static_pages#help'        , as: 'help'
-  get '/convention'     , to: 'static_pages#convention'  , as: 'convention'
+  get '/dashboard',       to: 'static_pages#dashboard',   as: 'dashboard'
+  get '/about',           to: 'static_pages#about',       as: 'about'
+  get '/help',            to: 'static_pages#help',        as: 'help'
+  get '/convention',      to: 'static_pages#convention',  as: 'convention'
 
   # User用ルーティング
   resources 'users'
@@ -13,9 +13,9 @@ Rails.application.routes.draw do
   resources 'events'
 
   # Sessions用ルーティング
-  get     '/login',      to: 'sessions#new'
-  post    '/login',      to: 'sessions#create'
-  delete  '/logout/:id', to: 'sessions#destroy'
+  get     '/login',        to: 'sessions#new'
+  post    '/login',        to: 'sessions#create'
+  delete  '/logout/:id',   to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
