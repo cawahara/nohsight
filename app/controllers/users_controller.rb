37aef571a41a598087bdb801004ff1946d71a7c3
Takahiro <tsolvalouy@gmail.com>
2017-05-21 13:54:10 +0000

    Create User controller system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,18 +15,33 @@ class UsersController < ApplicationController
    def create
       @user = User.new(user_params)
       if @user.save
-         flash[:success] = "ユーザー登録が完了しました"
+         flash['success'] = "ユーザー登録が完了しました"
          redirect_to(dashboard_url)
       else
-         flash[:danger] = "入力情報に不備があります。"
+         flash['danger'] = "入力情報に不備があります"
          render 'users/new'
       end
    end
 
    def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+         flash['success'] = "ユーザー情報を更新しました"
+         redirect_to(dashboard_url)
+      else
+         flash['danger'] = "入力情報不備があります"
+         render 'users/edit'
+      end
    end
 
    def destroy
+      @user = USer.find(params[:id])
+      if @user.destroy
+         flash['info'] = "ユーザー情報を削除しました"
+      else
+         flash['danger'] = "ユーザー情報を削除できません"
+      end
+      redirect_to(dashboard_url)
    end
 
    private
