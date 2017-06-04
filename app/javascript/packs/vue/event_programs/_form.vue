3de45281fd2429f634a0a00ac77ba0209bf3e30b
Takahiro <tsolvalouy@gmail.com>
2017-06-04 15:09:35 +0000

    Add removing element action for new form on Event Program

--- app/javascript/packs/vue/event_programs/_form.vue
+++ app/javascript/packs/vue/event_programs/_form.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="new-item">
+   <div v-bind:id="getTagId(id)" class="new-item">
       <input v-bind:name="getItemId(id, 'type')" type="hidden" value="create">
       <h3>新規追加演目</h3>
       <div class="edit-item">
@@ -20,6 +20,9 @@
             <input v-bind:name="getItemId(id, 'performer')" type="text">
          </div>
       </div>
+      <ul class="item-icons">
+         <li v-on:click="removeItem(id)">取消<span class="btn"><i class="fa fa-minus"></i></span></li>
+      </ul>
    </div>
 
 </template>
@@ -33,8 +36,15 @@
          }
       },
       methods: {
+         getTagId: function(id){
+            return 'ev_program-new-' + id
+         },
          getItemId: function(id, name){
             return 'ev_program[new-' + id + '][' + name + ']'
+         },
+         removeItem: function(id){
+            var el = document.getElementById('ev_program-new-' + id)
+            el.parentNode.removeChild(el)
          }
       }
    }
