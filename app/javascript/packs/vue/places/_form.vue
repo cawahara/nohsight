2187eba0836634c818a5f231655852ca3b086314
Shantti_Y <tsolvalouy@gmail.com>
2017-06-10 00:27:42 +0000

    Design additional edit form on EventProgram

--- app/javascript/packs/vue/places/_form.vue
+++ app/javascript/packs/vue/places/_form.vue
@@ -1,16 +1,19 @@
 <template>
-   <div class="form-item place-form">
+   <div>
+      <div class="form-pointer"></div>
+      <div class="form-item sub-form place-form">
+         <h3 class="form-header">■演目舞台の詳細情報</h3>
+         <div class="lg-form">
+            <label>住所</label>
+            <input v-bind:name="getColumn(id, 'address')" type="text" v-bind:value="place.address">
+         </div>
 
-      <div class="lg-form">
-         <label>住所</label>
-         <input v-bind:name="getColumn(id, 'address')" type="text" v-bind:value="place.address">
+         <div class="lg-form">
+            <label>サイトURL</label>
+            <input v-bind:name="getColumn(id, 'official_url')" type="text" v-bind:value="place.official_url">
+         </div>
+
       </div>
-
-      <div class="lg-form">
-         <label>サイトURL</label>
-         <input v-bind:name="getColumn(id, 'official_url')" type="text" v-bind:value="place.official_url">
-      </div>
-
    </div>
 </template>
 
--- app/javascript/packs/vue/programs/_form.vue
+++ app/javascript/packs/vue/programs/_form.vue
@@ -1,36 +1,39 @@
 <template>
-   <div class="form-item program-form">
+   <div>
+      <div class="form-pointer"></div>
+      <div class="form-item sub-form program-form">
+         <h3 class="form-header">■演目の詳細情報</h3>
+         <ul class="list-form">
+            <li class="with-category">
+               <label>曲目</label>
+               <input v-bind:name="getColumn(id, 'category')" type="text" v-bind:value="program.category">
+            </li>
+            <li class="with-duration">
+               <label>公演時間</label>
+               <input v-bind:name="getColumn(id, 'duration')" type="text" v-bind:value="program.duration">
+            </li>
+            <li class="with-shimai">
+               <label>仕舞</label>
+               <input v-bind:name="getColumn(id, 'shimai')" type="checkbox" v-bind:value="program.shimai"
+                v-bind:true-value="true" v-bind:false-value="false">
+            </li>
+         </ul>
 
-      <ul class="list-form">
-         <li class="with-category">
-            <label>曲目</label>
-            <input v-bind:name="getColumn(id, 'category')" type="text" v-bind:value="program.category">
-         </li>
-         <li class="with-duration">
-            <label>公演時間</label>
-            <input v-bind:name="getColumn(id, 'duration')" type="text" v-bind:value="program.duration">
-         </li>
-         <li class="with-shimai">
-            <label>仕舞</label>
-            <input v-bind:name="getColumn(id, 'shimai')" type="checkbox" v-bind:value="program.shimai"
-             v-bind:true-value="true" v-bind:false-value="false">
-         </li>
-      </ul>
-
-      <div class="lg-form">
-         <label>舞台</label>
-         <div class="input-with-btn">
-            <input v-bind:name="getColumn(id, 'place')" type="text" v-bind:value="place.title">
-            <span v-bind:class="{ active: places_edit_component }" class="btn" v-on:click="toggleComponent()"><i class="fa fa-map-marker"></i></span>
+         <div class="lg-form">
+            <label>舞台</label>
+            <div class="input-with-btn">
+               <input v-bind:name="getColumn(id, 'place')" type="text" v-bind:value="place.title">
+               <span v-bind:class="{ active: places_edit_component }" class="btn" v-on:click="toggleComponent()"><i class="fa fa-map-marker"></i></span>
+            </div>
          </div>
+
+         <places-edit
+            v-show="places_edit_component == true"
+            :inherit_id="id"
+            :inherit_place="place"
+         ></places-edit>
+
       </div>
-
-      <places-edit
-         v-show="places_edit_component == true"
-         :inherit_id="id"
-         :inherit_place="place"
-      ></places-edit>
-
    </div>
 </template>
 
