8de7c560450040d3094dfdf28bad5ee25e3887ee
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-07-05 11:58:09 +0000

    Organize Gemfile

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ end
 gem 'rails',                   '~> 5.0.2'
 gem 'puma',                    '~> 3.0'
 gem 'sass-rails',              '~> 5.0'
-gem 'uglifier',                '>= 1.3.0'
+gem 'uglifier',                '~> 1.3.0'
 gem 'coffee-rails',            '~> 4.2'
 gem 'jquery-rails'
 gem 'turbolinks',              '~> 5'
@@ -21,20 +21,20 @@ gem 'simple_enum',             '~> 2.3'                     # enumqを扱うヘ�
 gem 'kaminari'                                              # paginateヘルパー
 gem 'bcrypt',                  '~> 3.1.7'                   # 文字列(主にパスワード)のハッシュ化
 gem 'ransack',                 '~> 1.8'                     # ActiveREcordのレコード検索用ヘルパー
-gem 'mysql2',                  '~> 0.4.4'                   # 管理データベースをMySQLに移行
 gem 'seed_dump',               '>= 3.2.4'                   # データベース内レコードをseedに移行
 gem 'bootstrap-sass'                                        # bootstrap
 gem 'webpacker',               '~> 2.0'                     # WebPackをRailsで
+gem 'dotenv-rails',            '>= 2.2.1'                   # rails内に環境変数導入
 # gem 'redis', '~> 3.0'
-# gem 'therubyracer', platforms: :ruby
+gem 'therubyracer', platforms: :ruby
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
   # Add for debug (change from default gem: byebug)
   gem 'pry-byebug'
   gem 'pry-rails'
-  gem 'dotenv-rails',            '>= 2.2.1'                 # rails内に環境変数導入
   gem 'rack-cors', :require => 'rack/cors'
+  gem 'mysql2',                  '~> 0.4.4'                   # 管理データベースをMySQLに移行
 end
 
 group :development do
@@ -50,6 +50,10 @@ group :test do
    gem 'rails-controller-testing',    '0.1.1'            # assigns関数の追加
    gem 'minitest-reporters',          '~> 1.1.9'         # テスト結果をプログレスバーで表示
    gem 'timecop'                                         # テスト環境で時間操作
+end
+
+group :production do
+   gem 'pg', '~> 0.21.0'
 end
 
 # デフォルトで配備
--- db/schema.rb
+++ db/schema.rb
@@ -13,100 +13,82 @@
 ActiveRecord::Schema.define(version: 20170514013703) do
 
   create_table "event_performers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "event_program_id", null: false
-    t.integer  "performer_id",     null: false
+    t.integer  "event_program_id"
+    t.integer  "performer_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-    t.index ["event_program_id"], name: "ev_performer_to_ev_program_id_idx", using: :btree
-    t.index ["performer_id"], name: "ev_performer_to_performer_id_idx", using: :btree
   end
 
   create_table "event_programs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "event_id",              null: false
-    t.integer  "program_id",            null: false
-    t.string   "style",                 null: false
-    t.string   "genre",      limit: 45, null: false
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-    t.index ["event_id"], name: "ev_program_to_event_idx", using: :btree
-    t.index ["program_id"], name: "ev_program_to_program_id_idx", using: :btree
+    t.integer  "event_id"
+    t.integer  "program_id"
+    t.string   "style"
+    t.string   "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "place_id"
-    t.integer  "user_id",                    null: false
-    t.string   "title",                      null: false
+    t.string   "title"
     t.datetime "start_date"
     t.datetime "end_date"
+    t.string   "ticket"
     t.text     "information",  limit: 65535
     t.string   "official_url"
-    t.boolean  "published",                  null: false
+    t.integer  "user_id"
+    t.boolean  "published"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.index ["place_id"], name: "event_to_place_id_idx", using: :btree
-    t.index ["user_id"], name: "event_to_user_idx", using: :btree
   end
 
   create_table "performers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "full_name",  null: false
+    t.string   "full_name"
     t.string   "last_name"
     t.string   "first_name"
     t.integer  "style_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["style_id"], name: "performer_to_style_id_idx", using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "places", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "title",        limit: 45, null: false
-    t.string   "address",                 null: false
+    t.string   "title"
+    t.string   "address"
     t.string   "official_url"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "programs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "place_id"
-    t.string   "title",      null: false
+    t.string   "title"
     t.string   "category"
     t.boolean  "shimai"
     t.integer  "duration"
+    t.integer  "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["place_id"], name: "program_to_place_idx", using: :btree
   end
 
   create_table "styles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "title",      null: false
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "tickets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "event_id",   null: false
-    t.string   "grade",      null: false
-    t.integer  "price",      null: false
+    t.integer  "event_id"
+    t.string   "grade"
+    t.integer  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "ticket_to_event_id_idx", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",            null: false
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["email"], name: "email_UNIQUE", unique: true, using: :btree
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
-  add_foreign_key "event_performers", "event_programs", name: "ev_performer_to_ev_program_id", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "event_performers", "performers", name: "ev_performer_to_performer_id", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "event_programs", "events", name: "ev_program_to_event_id", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "event_programs", "programs", name: "ev_program_to_program_id", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "events", "places", name: "event_to_place_id", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "events", "users", name: "event_to_user_id", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "performers", "styles", name: "performer_to_style_id", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "programs", "places", name: "program_to_place_id", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "tickets", "events", name: "ticket_to_event_id", on_update: :cascade, on_delete: :cascade
 end
