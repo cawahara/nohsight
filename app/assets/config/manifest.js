9cd5da8bf855d3d983cff4cf95e4a05dc5cc645e
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-07-07 10:43:27 +0000

    Add target for precompile to Heroku including fonts

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link_tree ../images
+//= link_directory ../fonts .otf
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-
-# include font assets
-Rails.application.config.assets.precompile << /\.(?:svg|otf|woff|ttf)$/
 |
