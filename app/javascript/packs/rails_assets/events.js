a5307dd78bcfc8927e03454a854935fe9ea53e93
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-08-06 03:38:51 +0000

    Improve edit form on Event controller

--- app/javascript/packs/rails_assets/events.js
+++ app/javascript/packs/rails_assets/events.js
@@ -17,20 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
       format: 'Y/m/d'
    })
    $('#start-date').on('click', function(){
-      var end_date = $('#end-date').datetimepicker('getValue')
-      if(end_date){
-         $('#start-date').datetimepicker('setOptions', {maxDate: end_date, minDate: false})
-      }else{
-         $('#start-date').datetimepicker('setOptions', {maxDate: false, minDate: false})
-      }
-   })
-   $('#end-date').on('click', function(){
-      var start_date = $('#start-date').datetimepicker('getValue')
-      if(start_date){
-         $('#end-date').datetimepicker('setOptions', {minDate: start_date, maxDate: false})
-      }else{
-         $('#end-date').datetimepicker('setOptions', {minDate: false, maxDate: false})
-      }
+      var today = new Date(0)
+      $('#start-date').datetimepicker('setOptions', {maxDate: false, minDate: today.getTime()})
    })
    $.datetimepicker.setLocale('ja')
 })
