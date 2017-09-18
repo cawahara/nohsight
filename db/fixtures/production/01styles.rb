2f9a28b5a87668d3dd683b743ad76544cf7ffb8e
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-09-18 08:57:51 +0000

    Create style seeds on production

---
+++ db/fixtures/production/01styles.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+Style.seed do |s|
+   s.id     = 12
+   s.title  = 'シテ方観世流'
+end
+
+Style.seed do |s|
+   s.id     = 22
+   s.title  = 'シテ方宝生流'
+end
+
+Style.seed do |s|
+   s.id     = 32
+   s.title  = 'シテ方金春流'
+end
+
+Style.seed do |s|
+   s.id     = 42
+   s.title  = 'シテ方金剛流'
+end
+
+Style.seed do |s|
+   s.id     = 52
+   s.title  = 'シテ方喜多流'
+end
+
+Style.seed do |s|
+   s.id     = 62
+   s.title  = 'ワキ方高安流'
+end
+
+Style.seed do |s|
+   s.id     = 72
+   s.title  = 'ワキ方福王流'
+end
+
+Style.seed do |s|
+   s.id     = 82
+   s.title  = 'ワキ方宝生流'
+end
+
+Style.seed do |s|
+   s.id     = 92
+   s.title  = '狂言方大蔵流'
+end
+
+Style.seed do |s|
+   s.id     = 102
+   s.title  = '狂言方和泉流'
+end
+
+Style.seed do |s|
+   s.id     = 112
+   s.title  = '笛方一噌流'
+end
+
+Style.seed do |s|
+   s.id     = 122
+   s.title  = '笛方森田流'
+end
+
+Style.seed do |s|
+   s.id     = 132
+   s.title  = '笛方藤田流'
+end
+
+Style.seed do |s|
+   s.id     = 142
+   s.title  = '小鼓方幸流'
+end
+
+Style.seed do |s|
+   s.id     = 152
+   s.title  = '小鼓方幸清流'
+end
+
+Style.seed do |s|
+   s.id     = 162
+   s.title  = '小鼓方大倉流'
+end
+
+Style.seed do |s|
+   s.id     = 172
+   s.title  = '小鼓方観世流'
+end
+
+Style.seed do |s|
+   s.id     = 182
+   s.title  = '大鼓方葛野流'
+end
+
+Style.seed do |s|
+   s.id     = 192
+   s.title  = '大鼓方高安流'
+end
+
+Style.seed do |s|
+   s.id     = 202
+   s.title  = '大鼓方石井流'
+end
+
+Style.seed do |s|
+   s.id     = 212
+   s.title  = '大鼓方大倉流'
+end
+
+Style.seed do |s|
+   s.id     = 222
+   s.title  = '大鼓方観世流'
+end
+
+Style.seed do |s|
+   s.id     = 232
+   s.title  = '太鼓方観世流'
+end
+
+Style.seed do |s|
+   s.id     = 242
+   s.title  = '太鼓方金春流'
+end
