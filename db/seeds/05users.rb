bdb6749ab63b271582a68597444d51cb9b91b7f8
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-09-18 08:02:42 +0000

    Remove asset files

--- db/seeds/05users.rb
+++ db/seeds/05users.rb
@@ -1,17 +1,3 @@
 User.create!([
-  {name: "good example", email: "example@gmail.com", information: nil, password_digest: "$2a$10$If3v20WRuqY9lZWkfa3GPuOJwwx.KAihgrJ1nYoQUl6GUO4VXpR8S"},
-  {name: "gen uchida", email: "uchida@gmail.com", information: nil, password_digest: "$2a$10$y.CXy7DGsFKFfAQqfQq1n./l2aQlNGElrko0yFbzdqLiTsSHOt1YS"},
-  {name: "yasu takagi", email: "takagi@gmail.com", information: nil, password_digest: "$2a$10$hH2W67gAlrhTfV4bFMWVC.sxJi6z5YNoUf5CUTRQpUqJvNuFXcxWa"},
-  {name: "ai ouchi", email: "ouchi@gmail.com", information: nil, password_digest: "$2a$10$hBTjFBQHLUjavbpw.mOViecnKftSORF/1unY3dfgYHbls0x.orPlW"},
-  {name: "fumihisa taguchi", email: "taguchi@gmail.com", information: nil, password_digest: "$2a$10$A6UMnu6YYLkGcpF3rJGGE.X.37ril2eKr5e.zXWUIIiIvtwEYLCjO"},
-  {name: "goro yoshida", email: "yoshida@gmail.com", information: nil, password_digest: "$2a$10$ssJzldzgcZ93534.T3n85u3rxHkB8elEItDGodRUPpKSFaZEuGfLW"},
-  {name: "johny honda", email: "honda@gmail.com", information: nil, password_digest: "$2a$10$2hpotimDbzxg4aj7VR15jeqf0skN6e1un9VYMyNAUm6.t5w2q86gK"},
-  {name: "eiji ito", email: "ito@gmail.com", information: nil, password_digest: "$2a$10$SqrCbj0Q7XqQDCOucfjN1.0ZxVJuuI6en52g4HHRqCucORWomeE12"},
-  {name: "junta tanaka", email: "tanaka@gmail.com", information: nil, password_digest: "$2a$10$FOk.sxLbHaX4.X0TVvwtnu9.T4VtKjCvDnAsqzEWaki/gLD8A6HA."},
-  {name: "maruko okamoto", email: "okamoto@gmail.com", information: nil, password_digest: "$2a$10$MbkDK9UjbA2iXIiswZHk0u9o8VodjNNc02p9ZlgAbvZuT32wZzaoS"},
-  {name: "shinichiro higashihara", email: "higashihara@gmail.com", information: nil, password_digest: "$2a$10$ULuLosrl1d9IoKnDr6g.d.mMFX6SBqnFHa/E.2bwKXBrhH.h4XsUi"},
-  {name: "sota otuska", email: "otuska@gmail.com", information: nil, password_digest: "$2a$10$1zQ9elsufZ07zfa3ptk.q.XpZxN4mmzNNMTSEl6wptRs2bdgEhA4W"},
-  {name: "shun kaito", email: "kaito@gmail.com", information: nil, password_digest: "$2a$10$ux9GFA3YMkHhyOli.sqHseqLlwOENMCLZM3VZAS1eRsT.T5iZRY1S"},
-  {name: "junta nakata", email: "nakata@gmail.com", information: nil, password_digest: "$2a$10$tP60yjBHrwWqiYLPlQboLexrp83jSokpGs4My0mbif9balFwKDgxy"},
-  {name: "hidekazu mukai", email: "mukai@gmail.com", information: nil, password_digest: "$2a$10$Fn7CNYoSM0do5HDdVP7yiOwyw3us2/gXUUHTH6HB.zZVWxaLEMxYS"}
+  {name: "tester", email: "test@gmail.com", information: nil, password_digest: "$2a$10$H2IJeeX2uoWmOtM1NZxelO4eruYiidENCJwNSSs/1l50azzAbaWYC"}
 ])
