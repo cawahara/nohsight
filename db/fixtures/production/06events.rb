f6c223015928aaa556eaedce016770c0be6088db
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-09-30 04:26:23 +0000

    Re-write Event and User seed by adding publishing_status and is_admin field respectively

--- db/fixtures/production/06events.rb
+++ db/fixtures/production/06events.rb
@@ -9,7 +9,7 @@ Event.seed do |s|
 	s.information = '虚飾を削ぎ落としたミニマルな舞台装置の上で、己が肉体を駆使して演じることでその背後にある幽玄なる世界観までをも、観る者に五感を通じて体感させることができる能や狂言など、悠久の歴史を超えて育まれてきた日本の伝統芸能。その高尚さゆえ客層が硬直化し、なかなか身近な存在になりえなかったそれらを現代にふさわしいスタイルで体験できるプロジェクト、それが"see the invisible。"'
 	s.official_url = 'http://seetheinvisible.jp/'
 	s.category = '能楽協会主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -21,7 +21,7 @@ Event.seed do |s|
 	s.information = ''
 	s.official_url = 'http://www.kongou-net.com/schedule/20170924.html'
 	s.category = 'その他'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -33,7 +33,7 @@ Event.seed do |s|
 	s.information = ''
 	s.official_url = 'http://junseikai.or.tv/future/222/'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -45,7 +45,7 @@ Event.seed do |s|
 	s.information = ''
 	s.official_url = 'http://www.kongou-net.com/schedule/index.html'
 	s.category = 'その他'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -57,7 +57,7 @@ Event.seed do |s|
 	s.information = '終演約1時間後、能楽堂向かいの京都御苑より大文字の送り火がご覧頂けます。（京都御苑は入場自由です）'
 	s.official_url = 'http://www.kongou-net.com/schedule/20170816.html'
 	s.category = 'その他'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -69,7 +69,7 @@ Event.seed do |s|
 	s.information = ''
 	s.official_url = 'http://www.kanazawanohgakukai.jp/pdf/teirei2017_10.pdf'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -81,7 +81,7 @@ Event.seed do |s|
 	s.information = ''
 	s.official_url = 'http://kanze.net/publics/index/79/'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -93,7 +93,7 @@ Event.seed do |s|
 	s.information = '※事務局で許可した以外の方の写真・ビデオ撮影・録音はお断りいたします。\n※場内では携帯電話などの電源はお切りください\n※車でお越しの方は、京都観世会館東隣の湯量駐車場をご利用ください。万社の場合は平安神宮前の市営有料駐車場をご利用ください。'
 	s.official_url = 'http://www.kyoto-kanze.jp/performanceguide/urata_teikinoh_front.jpg'
 	s.category = '能楽協会主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -105,7 +105,7 @@ Event.seed do |s|
 	s.information = '※特別会員席以外の座席兼は、渡欧実午前10字より先着順にお引替えいたします。\n※開演中のお出入りはなるべくご遠慮ください。\n※許可なき写真殺意、録音、録画はお断りいたします。\n※場内では携帯電話等の呼び出し音をお切りください。\n※予告なく出演者が変更となる場合がございますので、あらかじめご了承ください。\n※車でお越しのお客様には、会館左隣に有料駐車場がございます。また満車の際には岡崎公園市営地下駐車場もご利用いただけますが、近隣の催し等により、込み合う場合もぼざいますので、ご来館はなるべく公共の交通機関をご利用ください。'
 	s.official_url = 'http://www.kyoto-kanze.jp/reikai/reikai9-2_front.jpg'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -117,7 +117,7 @@ Event.seed do |s|
 	s.information = '井伊直弼を偲んで毎年開催する「横浜かもんやま能」。第34回を迎える今年は、井伊家お抱えの狂言方だった茂山家の茂山千作が狂言「萩大名」を、能は観世銕之丞が、虚偽の告げ口により勘当されて盲目となった俊徳丸と父との再会を、難波の浦の心象風景とともに美しく描き出した名曲「弱法師」を小書（特殊演出）「盲目之舞」でお送りいたします'
 	s.official_url = 'http://www.welcome.city.yokohama.jp/ja/tourism/eventinfo/ev_detail.php?bid=yw3664'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -129,7 +129,7 @@ Event.seed do |s|
 	s.information = '２０１７年は、華道・池坊の開祖と呼ばれる池坊専慶がいけばなの名手として文献に登場してから５５５年に当たります。これを記念し、花に関係する５つの能を上演し、次期家元の池坊専好が舞台を花で彩ります。「花」という共通のキーワードを持つ近しい存在でありながらその交流が限られてきた「能」と「いけばな」。両者が競演し、新たな「花」を創造します。'
 	s.official_url = 'http://ynt.yafjp.org/schedule/?p=2144'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -141,7 +141,7 @@ Event.seed do |s|
 	s.information = '２０１７年は、華道・池坊の開祖と呼ばれる池坊専慶がいけばなの名手として文献に登場してから５５５年に当たります。これを記念し、花に関係する５つの能を上演し、次期家元の池坊専好が舞台を花で彩ります。「花」という共通のキーワードを持つ近しい存在でありながらその交流が限られてきた「能」と「いけばな」。両者が競演し、新たな「花」を創造します。'
 	s.official_url = 'http://ynt.yafjp.org/schedule/?p=2149'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -153,7 +153,7 @@ Event.seed do |s|
 	s.information = '２０１７年は、華道・池坊の開祖と呼ばれる池坊専慶がいけばなの名手として文献に登場してから５５５年に当たります。これを記念し、花に関係する５つの能を上演し、次期家元の池坊専好が舞台を花で彩ります。「花」という共通のキーワードを持つ近しい存在でありながらその交流が限られてきた「能」と「いけばな」。両者が競演し、新たな「花」を創造します。'
 	s.official_url = 'http://ynt.yafjp.org/schedule/?p=2155'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -165,7 +165,7 @@ Event.seed do |s|
 	s.information = '２０１７年は、華道・池坊の開祖と呼ばれる池坊専慶がいけばなの名手として文献に登場してから５５５年に当たります。これを記念し、花に関係する５つの能を上演し、次期家元の池坊専好が舞台を花で彩ります。「花」という共通のキーワードを持つ近しい存在でありながらその交流が限られてきた「能」と「いけばな」。両者が競演し、新たな「花」を創造します。'
 	s.official_url = 'http://ynt.yafjp.org/schedule/?p=2158'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -177,7 +177,7 @@ Event.seed do |s|
 	s.information = '２０１７年は、華道・池坊の開祖と呼ばれる池坊専慶がいけばなの名手として文献に登場してから５５５年に当たります。これを記念し、花に関係する５つの能を上演し、次期家元の池坊専好が舞台を花で彩ります。「花」という共通のキーワードを持つ近しい存在でありながらその交流が限られてきた「能」と「いけばな」。両者が競演し、新たな「花」を創造します。'
 	s.official_url = 'http://ynt.yafjp.org/schedule/?p=2161'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -189,7 +189,7 @@ Event.seed do |s|
 	s.information = '山本能楽堂９０周年記念　とくい能特別公演「卒都婆小町」を開催いたします。\n＜移ろいゆくもののはかなさ…能が描く「究極の世界」を感じてください。＞\n「卒都婆小町」は、能の世界で大変重く扱われる「老女物（ろうじょもの・老女を主役とする能）」の一つです。長い修行の末のにじみでるような味が要求され、老女物を演じることは、能楽師が目指す終極の目的になっています。「卒都婆小町」では、才色兼備で誉れの高かった小野小町が、年老いて、落ちぶれた姿で描かれています。しかし、それでもなお気位は高く、高僧を相手に禅問答をし、昔、小町に恋して死んでしまった深草ノ少将の怨霊がのり移って狂乱します。老いるとは何なのか…、驕慢の美人のなれの果てを描きながらも、淡々として、移ろいゆくもののはかなさ、人生の奥深さがそこには描かれています。見る方それぞれの心に深く響く、能が描く究極の世界がそこには広がっています。'
 	s.official_url = 'http://www.noh-theater.com/nfblog/?p=541'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -201,7 +201,7 @@ Event.seed do |s|
 	s.information = '日本古来の詞章を謡い、美しい日本の伝統、能の楽しさを体感しましょう。'
 	s.official_url = 'http://www.noh-theater.com/nfblog/?p=595'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -213,7 +213,7 @@ Event.seed do |s|
 	s.information = ''
 	s.official_url = 'http://www.ntj.jac.go.jp/schedule/nou/2017/10197.html?lan=j'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -225,7 +225,7 @@ Event.seed do |s|
 	s.information = ''
 	s.official_url = 'http://www.ntj.jac.go.jp/schedule/nou/2017/10198.html?lan=j'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
 
 Event.seed do |s|
@@ -237,5 +237,5 @@ Event.seed do |s|
 	s.information = '同一作品を流儀や小書による違った演出でお楽しみいただくシリーズ。今回は流儀により登場人物や展開が異なる狂言「御茶の水（水汲）」、養老の滝伝説を扱った能「養老」、水の流れが描かれた二作品をお届けします。'
 	s.official_url = 'http://www.ntj.jac.go.jp/schedule/nou/2017/10199.html?lan=j'
 	s.category = '能楽堂主催'
-	s.published = true
+	s.publishing_status = 3
 end
