9adc128d619c30201627d1effcce6275d510cbd9
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-10-11 14:56:02 +0000

    Edit view showing each user's point records

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,10 @@ class User < ApplicationRecord
                                        allow_nil: false,
                                        on: :create
 
+   def points
+      return self.point_records.pluck(:point).inject(0) { |sum, p| sum + p }
+   end
+
    def self.digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                           BCrypt::Engine.cost
