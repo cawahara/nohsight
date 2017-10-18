9c2acad3f8beea042f1c66f8c323234fa73665b4
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-10-18 11:16:57 +0000

    Fixing hendling input params at validation stage

--- app/controllers/concerns/crud_event.rb
+++ app/controllers/concerns/crud_event.rb
@@ -32,6 +32,90 @@ module CRUDEvent
       end
    end
 
+   def params_valid?
+      @invalid_occured = false
+      place = get_master_data(Place, 'title', @place_params)
+      @error_msgs[:place] = get_error_msgs(place)
+      @event_params.merge!(place_id: place.id)
+      event = Event.new(@event_params)
+      unless event.valid?
+         @error_msgs[:event] = get_error_msgs(event)
+         @invalid_occured = true
+      end
+      @error_msgs[:event_programs] = {}
+      @event_programs_params.values.each.with_index do |ev_program_params, ev_program_idx|
+         ev_program_error = {}
+         ev_program_error[:event_performers] = {}
+         if ev_program_params[:mode] == 'destroy'
+            @error_msgs[:event_programs][:"#{ev_program_idx}"] = ev_program_error
+            next
+         end
+         ev_program_params[:event_performers].values.each.with_index do |ev_performer_params, ev_performer_idx|
+            if ev_performer_params[:mode] == 'destroy'
+               ev_program_error[:event_performers][:"#{ev_performer_idx}"] = {}
+               next
+            end
+            init_params = ev_performer_params.reject{|key, val| !EventPerformer.column_names.include?(key.to_s)}
+            performer = get_master_data(Performer, 'full_name', ev_performer_params)
+            ev_performer_error = get_error_msgs(performer)
+            ev_performer_params.merge!({ performer_id: performer.id })
+            init_params.merge!({ event_program_id: 0, performer_id: ev_performer_params[:performer_id] })
+            ev_performer = EventPerformer.new(init_params)
+            @invalid_occured = true unless ev_performer.valid?
+            ev_program_error[:event_performers][:"#{ev_performer_idx}"] = ev_performer_error
+         end
+         init_params = ev_program_params.reject{|key, val| !EventProgram.column_names.include?(key.to_s)}
+         program = get_master_data(Program, 'title', ev_program_params)
+         ev_program_error.merge!(get_error_msgs(program))
+         ev_program_params.merge!({ program_id: program.id })
+         init_params.merge!({ event_id: 0, program_id: ev_program_params[:program_id] })
+         ev_program = EventProgram.new(init_params)
+         unless ev_program.valid?
+             ev_program_error.merge!(ev_program.errors.messages)
+             @invalid_occured = true
+         end
+         @error_msgs[:event_programs][:"#{ev_program_idx}"] = ev_program_error
+      end
+      @error_msgs[:tickets] = {}
+      @tickets_params.values.each.with_index do |ticket_params, ticket_idx|
+         if ticket_params[:mode] == 'destroy'
+            @error_msgs[:tickets][:"#{ticket_idx}"] = {}
+            next
+         end
+         init_params = ticket_params.reject{|key, val| !Ticket.column_names.include?(key.to_s)}
+         init_params[:event_id] = 0
+         ticket = Ticket.new(init_params)
+         unless ticket.valid?
+            @error_msgs[:tickets][:"#{ticket_idx}"] = ticket.errors.messages
+            @invalid_occured = true
+         end
+      end
+      raise ActiveRecord::RecordInvalid if @invalid_occured == true
+   end
+
+   def get_error_msgs(record)
+      if record.errors.any?
+         return record.errors.messages
+      else
+         return {}
+      end
+   end
+
+   private
+
+   def get_master_data(model, key, params)
+      record = model.find_or_initialize_by("#{key}": params[:"#{key}"]) do |record|
+         record.attributes = params.reject{|key, val| !model.column_names.include?(key.to_s)}
+         record.id = nil
+      end
+      if record.valid?
+         record.save
+      else
+         @invalid_occured = true
+      end
+      return record
+   end
+
    def initialize_event(event, mode)
       if mode == 'create'
          begin
@@ -63,68 +147,6 @@ module CRUDEvent
    def update_tickets(event)
       @tickets_params.values.each do |ticket_params|
          ticket = update_relative_data_attributes(ticket_params, Ticket, event)
-      end
-   end
-
-   def params_valid?
-      @invalid_occured = false
-      @error_msgs[:place] = {}
-      @event_params.merge!(place_id: create_master_data(Place, 'title', @place_params, @error_msgs[:place]))
-      binding.pry
-      validated_event = Event.new(@event_params)
-      @error_msgs[:event] = validated_event.errors.messages
-      @invalid_occured = true  unless validated_event.valid?
-
-      @error_msgs[:event_programs] = {}
-      @event_programs_params.values.each.with_index do |ev_program_params, ev_program_idx|
-         ev_program_error = {}
-         ev_program_error[:event_performers] = {}
-         init_params = ev_program_params.reject{|key, val| !EventProgram.column_names.include?(key.to_s)}
-         ev_program_params.merge!({ program_id: create_master_data(Program, 'title', ev_program_params, ev_program_error) })
-         init_params.merge!({ event_id: 0, program_id: ev_program_params[:program_id] })
-         ev_program = EventProgram.new(init_params)
-
-         ev_program_params[:event_performers].values.each.with_index do |ev_performer_params, ev_performer_idx|
-            ev_performer_error = {}
-            init_params = ev_performer_params.reject{|key, val| !EventPerformer.column_names.include?(key.to_s)}
-            ev_performer_params.merge!({ performer_id: create_master_data(Performer, 'full_name', ev_performer_params, ev_performer_error) })
-            init_params.merge!({ event_program_id: 0, performer_id: ev_performer_params[:performer_id] })
-            ev_performer = EventPerformer.new(init_params)
-            ev_program_error[:event_performers][:"#{ev_performer_idx}"] = ev_performer_error
-         end
-         unless ev_program.valid?
-             @invalid_occured = true
-             ev_program_error.merge!(ev_program.errors.messages)
-         end
-         @error_msgs[:event_programs][:"#{ev_program_idx}"] = ev_program_error
-      end
-
-      @error_msgs[:tickets] = {}
-      @tickets_params.values.each.with_index do |ticket_params, ticket_idx|
-         ticket_error = {}
-         init_params = ticket_params.reject{|key, val| !Ticket.column_names.include?(key.to_s)}
-         init_params[:event_id] = 0
-         ticket = Ticket.new(init_params)
-         @invalid_occured = true unless ticket.valid?
-         @error_msgs[:tickets][:"#{ticket_idx}"] = ticket.errors.messages
-      end
-      raise ActiveRecord::RecordInvalid if @invalid_occured == true
-   end
-
-   private
-
-   def create_master_data(model, key, params, model_error = nil)
-      record = model.find_or_initialize_by("#{key}": params[:"#{key}"]) do |record|
-         record.attributes = params.reject{|key, val| !model.column_names.include?(key.to_s)}
-         record.id = nil
-      end
-      if record.valid?
-         record.save
-         return record.id
-      else
-         model_error = record.errors.messages
-         @invalid_occured = true
-         return nil
       end
    end
 
