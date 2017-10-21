86cf9e5a851975d1f0dadad14fd0ca369a2e2811
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-10-21 08:41:51 +0000

    Improve migration files for updating that their columns are upside down

--- db/migrate/20171021081422_change_column_to_user.rb
+++ db/migrate/20171021081422_change_column_to_user.rb
@@ -1,5 +1,5 @@
 class ChangeColumnToUser < ActiveRecord::Migration[5.0]
    def up
-      change_column :events, :category, :string, default: 'その他'
+      change_column :users, :is_admin, :boolean, default: false
    end
 end
--- db/migrate/20171021081427_change_column_to_event.rb
+++ db/migrate/20171021081427_change_column_to_event.rb
@@ -1,5 +1,5 @@
 class ChangeColumnToEvent < ActiveRecord::Migration[5.0]
-  def up
-     change_column :users, :is_admin, :boolean, default: false
-  end
+   def up
+      change_column :events, :category, :string, default: 'その他'
+   end
 end
