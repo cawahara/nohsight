ea83d2d68f242da07a67ed63f0f267cf81f478ce
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-11-08 12:51:02 +0000

    Remove datetimepicker on jquery npm

--- app/javascript/packs/rails_assets/static_pages.js
+++ app/javascript/packs/rails_assets/static_pages.js
@@ -1,27 +1,11 @@
 import * as $ from 'jquery'
-require('jquery-datetimepicker/build/jquery.datetimepicker.full.min')
 
 document.addEventListener('DOMContentLoaded', () => {
 
    const target_content = document.getElementById('main').children[0]
    if(target_content == document.getElementById('search') ||
       target_content == document.getElementById('dashboard')){
-      $('.datepicker-for-search').datetimepicker({
-         format: 'Y/m/d',
-         timepicker: false
-      })
-
-      // for detailed search form
-      $('#start-date').on('click', function(){
-         var date_value = document.getElementById('end-date').value
-         var date_value_for_picker = date_value != '' ? Date.parse(date_value) : false
-         $('#start-date').datetimepicker('setOptions', {maxDate: date_value_for_picker, minDate: false})
-      })
-      $('#end-date').on('click', function(){
-         var date_value = document.getElementById('start-date').value
-         var date_value_for_picker = date_value != '' ? Date.parse(date_value) : false
-         $('#end-date').datetimepicker('setOptions', {maxDate: false, minDate: date_value_for_picker})
-      })
-      $.datetimepicker.setLocale('ja')
+      // REVIEW: 現状、defaultのrailsヘルパーで日付フォームを実装しているが、
+      //         Material dateimepickerが実装できたらそちらに変更したい
    }
 })
