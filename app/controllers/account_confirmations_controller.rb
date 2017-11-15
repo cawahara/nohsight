d94131de6b5410eb76c57e9018aae6b72122ff9e
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-11-15 14:51:25 +0000

    Add and edit account activation action to confirm user

---
+++ app/controllers/account_confirmations_controller.rb
@@ -0,0 +1,16 @@
+class AccountConfirmationsController < ApplicationController
+
+   def new
+      user = User.find_by(email: params[:email])
+      if user && !user.confirmed? && user.authenticated?('confirmation', params[:token])
+         user.update_attributes!(confirmed: true, confirmed_at: Time.zone.now)
+         login(user)
+         flash[:success] = 'ユーザー登録が完了しました！NohSightへようこそ！'
+         redirect_to user_url(user)
+      elsif user.nil? || !user.authenticated?('confirmation', params[:token])
+         flash[:danger] = "無効なリンクURLです"
+      elsif user.confirmed?
+         flash[:danger] = "メールアドレス「#{user.email}」はすでに登録完了されています"
+      end
+   end
+end
