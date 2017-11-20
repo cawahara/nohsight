40031c40f40c06c58f73644f6dc493ceb2219e7b
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-11-20 15:16:48 +0000

    Add join_history paths

--- config/routes.rb
+++ config/routes.rb
@@ -71,5 +71,8 @@ Rails.application.routes.draw do
    get      '/feedback', to: 'feedbacks#new'
    post     '/feedback', to: 'feedbacks#create'
 
+   # JoinHistory用ルーティング
+   resources   'join_histories', only: [:index, :update, :destroy]
+
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
