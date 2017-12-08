b23d150f1dc538466c7b7840578be85ae4aee055
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-12-08 14:42:00 +0000

    Run  for creating Rails5.1.4 environment

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'boot'
 
 require 'rails/all'
@@ -8,29 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-
 module NohEvent
-   class Application < Rails::Application
-      # Settings in config/environments/* take precedence over those specified here.
-      # Application configuration should go into files in config/initializers
-      # -- all .rb files in that directory are automatically loaded.
-   end
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
 
-   Rails.application.configure do
-      config.assets.paths << config.root.join('node_modules')
-      config.serve_static_assets = true
-      config.assets.initialize_on_precompile = true
-
-      config.generators do |g|
-         g.test_framework :rspec,
-                          fixtures: true,
-                          view_specs: false,
-                          helper_specs: false,
-                          routing_specs: false,
-                          controller_specs: true,
-                          request_specs: false
-         g.fixture_replacement :factory_bot, dir: 'spec/factories'
-      end
-   end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
 end
